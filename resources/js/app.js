c76aacc0e4086957cef7610bbd30e3d07c31d9ea
MaTiUs77 <matius77@gmail.com>
2019-10-10 12:05:23 -0300

    Se remueve el archivo bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window._ = require('lodash');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
-require('./bootstrap');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.Vue = require('vue');
 
